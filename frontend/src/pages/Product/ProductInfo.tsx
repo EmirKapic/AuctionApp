5ba52b51765188294962164cc20d94043d244341
Emir Kapic <ekapic1@etf.unsa.ba>
2023-10-20 11:32:52 +0200

    added input field and bid button

--- frontend/src/pages/Product/ProductInfo.tsx
+++ frontend/src/pages/Product/ProductInfo.tsx
@@ -1,3 +1,4 @@
+import Button from "components/Common/Button";
 import ProductTitle from "components/Common/ProductTitle";
 import Product from "models/Product";
 import { getNormalized } from "services/DateParser";
@@ -12,7 +13,7 @@ interface WeeksAndDays {
 }
 
 function calculateTimeLeft(date: Date): WeeksAndDays {
-  const millis = +date.getTime() - +new Date().getTime();
+  const millis = date.getTime() - new Date().getTime();
   const days = Math.round(millis / (1000 * 60 * 60 * 24)) + 1;
   return { weeks: Math.round(days / 7), days: days % 7 };
 }
@@ -45,6 +46,22 @@ export default function ProductInfo(props: ProductInfoProps) {
           </span>
         </div>
       </section>
+
+      <section className="flex gap-4 h-min mb-8 hidden">
+        <input
+          type="number"
+          placeholder="Enter 56$ or higher"
+          className="outline outline-gray-200 indent-4 py-4 flex-grow"
+          min={props.product.highestBid + 1}
+        />
+        <Button
+          text="place bid"
+          className="lg:flex-shrink-0 text-sm font-bold border-2 border-purple px-8 py-2 flex justify-center gap-2 items-center duration-300 
+          uppercase hover:bg-purple hover:text-white shadow-purple-md none"
+          onClick={() => {}}
+        />
+      </section>
+
       <div className="flex border-b-2 border-slate-200">
         <button className="text-purple border-b-2 border-purple px-8 py-2">
           Details
