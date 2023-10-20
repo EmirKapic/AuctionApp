11f93a35330a27b566f498aca4b2d02a7aebf764
Emir Kapic <ekapic1@etf.unsa.ba>
2023-10-20 11:47:31 +0200

    code cleanup

--- frontend/src/pages/LandingPage/Hero/Hero.tsx
+++ frontend/src/pages/LandingPage/Hero/Hero.tsx
@@ -1,17 +1,7 @@
-import ProductTitle from "components/Common/ProductTitle";
 import CategoriesList from "./CategoriesList";
-import PurpleButton from "components/Common/Button";
-import useFetchOne from "hooks/useFetchOne";
-import Product from "models/Product";
 import HeroProduct from "./HeroProduct";
 
 export default function Hero() {
-  const { data, isLoading } = useFetchOne<Product>(
-    "http://localhost:8080/api/products/random",
-  );
-
-  data;
-
   return (
     <section className="bg-lightgrey-100 w-full mb-10">
       <div className="max-w-container-lg w-full mx-auto flex">
