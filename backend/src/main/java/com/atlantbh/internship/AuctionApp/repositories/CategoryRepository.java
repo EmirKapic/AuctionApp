4bd5f85650d3b8443971bae04fdcd3ba0c753db1
Emir Kapic <ekapic1@etf.unsa.ba>
2023-10-20 14:54:58 +0200

    different jpa repository used

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/CategoryRepository.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
 package com.atlantbh.internship.AuctionApp.repositories;
 
 import com.atlantbh.internship.AuctionApp.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
