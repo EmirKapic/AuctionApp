214235ee89a98b2e76fdb33b851c8ff483a79505
Emir Kapic <ekapic1@etf.unsa.ba>
2023-10-25 14:34:32 +0200

    limit scroll to 4 pages

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
 export const defaultPageSize = 8;
+export const maxPagesSpecialOffers = 3;
 export const fallbackImageUrl =
   "https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
 export const pageSizeShop = 9;
