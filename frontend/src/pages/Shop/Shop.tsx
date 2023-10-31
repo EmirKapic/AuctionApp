441b6b2ba87f1475e0df52620554d79d884e85eb
Emir Kapic <ekapic1@etf.unsa.ba>
2023-10-31 10:45:09 +0100

    move fetched categories state up to shop

--- frontend/src/pages/Shop/Shop.tsx
+++ frontend/src/pages/Shop/Shop.tsx
@@ -4,9 +4,12 @@ import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import useFetchPage from "hooks/useFetchPage";
 import UrlBuilder from "services/UrlBuilder";
 import { useEffect, useState } from "react";
-import { pageSizeShop } from "constants";
+import { pageSizeShop } from "../../constants"; //it gets mixed up with "constants" package if absolute path
 import Product from "models/Product";
 import ProductList from "./ProductList/ProductList";
+import Breadcrumb, { BreadcrumbItem } from "components/Common/Breadcrumb";
+import useFetchAll from "hooks/useFetchAll";
+import CategoryWithSubs from "models/CategoryWithSubs";
 
 export default function Shop() {
   const [page, setPage] = useState(0);
@@ -18,6 +21,13 @@ export default function Shop() {
     undefined,
     queryParams,
   );
+
+  const {
+    data: categories,
+    isLoading: categoriesLoading,
+    isError: categoriesError,
+  } = useFetchAll<CategoryWithSubs>(new UrlBuilder().categories().url);
+
   const { state } = useLocation(); //in place of props, actual props cant be used for this due to being navigated to
   useEffect(() => {
     if (state && state.pageReset) {
@@ -26,27 +36,34 @@ export default function Shop() {
     }
   }, [state]);
 
-  if (isLoading) {
+  if (isLoading || categoriesLoading) {
     return <div>Loading data...</div>;
   }
-  if (isError) {
+  if (isError || categoriesError) {
     return <div>Error while fetching data...</div>;
   }
 
+  const breadCrumbItems: BreadcrumbItem[] = [];
+  if (queryParams.has("categoryId")) breadCrumbItems.push({ title: "" });
+
   return (
-    <Container type="large" className="flex gap-10">
-      <aside className="flex-shrink-0">
-        <ProductCategories
-          activeId={parseInt(queryParams.get("categoryId") || "-1")} //in case categoryId not present, -1 so no category is active
-        />
-      </aside>
-      <div className="flex-grow">
-        <ProductList
-          type="grid"
-          items={data}
-          handleNextPage={() => setPage(page + 1)}
-        />
-      </div>
-    </Container>
+    <div>
+      <Breadcrumb title="shop" items={[]} />
+      <Container type="large" className="flex gap-10">
+        <aside className="flex-shrink-0">
+          <ProductCategories
+            categories={categories}
+            activeId={parseInt(queryParams.get("categoryId") || "-1")} //in case categoryId not present, -1 so no category is active
+          />
+        </aside>
+        <div className="flex-grow">
+          <ProductList
+            type="grid"
+            items={data}
+            handleNextPage={() => setPage(page + 1)}
+          />
+        </div>
+      </Container>
+    </div>
   );
 }
--- frontend/src/pages/Shop/Sidebar/ProductCategories.tsx
+++ frontend/src/pages/Shop/Sidebar/ProductCategories.tsx
@@ -1,26 +1,14 @@
-import useFetchAll from "hooks/useFetchAll";
 import CategoryWithSubs from "models/CategoryWithSubs";
-import UrlBuilder from "services/UrlBuilder";
 import Sidebar from "./Sidebar";
 import CategoryListItem from "./CategoryListItem";
 
 export interface ProductCategoriesProps {
+  categories: Array<CategoryWithSubs>;
   activeId?: number;
 }
 
 export default function ProductCategories(props: ProductCategoriesProps) {
-  const { data, isLoading, isError } = useFetchAll<CategoryWithSubs>(
-    new UrlBuilder().categories().url,
-  );
-
-  if (isLoading) {
-    return <div>Loading categories...</div>;
-  }
-  if (isError) {
-    return <div>Error while fetching data...</div>;
-  }
-
-  const categories = data.map((category) => (
+  const categories = props.categories.map((category) => (
     <CategoryListItem
       category={category}
       active={category.id === props.activeId}
