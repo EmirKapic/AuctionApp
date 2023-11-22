b9f60df24440c21ab66bbe6d69283c7000dd9a9e
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-22 09:54:01 +0100

    add request/response models for login

---
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/dtos/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.atlantbh.internship.AuctionApp.dtos;
+
+public record LoginRequest(String email, String token) {
+}
---
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/dtos/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.atlantbh.internship.AuctionApp.dtos;
+
+public record LoginResponse(String email, String token) {
+}
