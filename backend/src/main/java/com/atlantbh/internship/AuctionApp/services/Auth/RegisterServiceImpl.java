9313d98c11e44745630fe1cea93ff84c7f31c2ff
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-22 14:03:27 +0100

    implement registerservice interface

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/services/Auth/RegisterServiceImpl.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/services/Auth/RegisterServiceImpl.java
@@ -1,4 +1,25 @@
 package com.atlantbh.internship.AuctionApp.services.Auth;
 
-public class RegisterServiceImpl {
+import com.atlantbh.internship.AuctionApp.models.User;
+import com.atlantbh.internship.AuctionApp.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegisterServiceImpl implements RegisterService{
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean registerUser(User user) {
+        try{
+            userRepository.save(user);
+            return true;
+        }
+        catch(DataIntegrityViolationException exception){
+            System.out.println("email already exists");
+            return false;
+        }
+    }
 }
