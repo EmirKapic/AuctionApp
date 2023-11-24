102d1e1a84d5179e192ab2c2915f2d141287fe77
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-24 15:23:18 +0100

    preauthorize added (not working)

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/config/JwtAuthorizationFilter.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/config/JwtAuthorizationFilter.java
@@ -10,12 +10,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Component
@@ -37,7 +39,9 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             if (claims != null && jwtService.validateExpiration(claims)){
                 String email = claims.getSubject();
                 Authentication authentication =
-                    new UsernamePasswordAuthenticationToken(email, "", new ArrayList<>());
+                    new UsernamePasswordAuthenticationToken(email, "", new ArrayList<>(
+                            List.of(new SimpleGrantedAuthority("user"))
+                    ));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
