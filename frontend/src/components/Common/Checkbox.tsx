41248fefbd4d6d53af5cae6e74d4dd13f72ab5d0
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-27 10:53:54 +0100

    make checkbox component

---
+++ frontend/src/components/Common/Checkbox.tsx
@@ -0,0 +1,22 @@
+export interface CheckboxProps {
+  label: string;
+  id: string;
+  onChange: (active: boolean) => void;
+  checked?: boolean;
+  labelClassName?: string;
+}
+
+export default function Checkbox(props: CheckboxProps) {
+  return (
+    <div className="flex gap-3">
+      <input
+        id={props.id}
+        type="checkbox"
+        onChange={(e) => props.onChange(e.target.checked)}
+      />
+      <label htmlFor={props.id} className={props.labelClassName}>
+        {[props.label]}
+      </label>
+    </div>
+  );
+}
