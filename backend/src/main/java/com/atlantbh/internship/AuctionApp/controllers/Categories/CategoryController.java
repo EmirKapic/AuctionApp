8529355b8d3458197e7b9e521ad48122d41bcc66
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-27 23:58:14 +0100

    remove unneeded authorize

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/controllers/Categories/CategoryController.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/controllers/Categories/CategoryController.java
@@ -3,7 +3,6 @@ package com.atlantbh.internship.AuctionApp.controllers.Categories;
 import com.atlantbh.internship.AuctionApp.dtos.CategoryDto;
 import com.atlantbh.internship.AuctionApp.services.Category.CategoryService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     private CategoryService categoryService;
     @GetMapping
-    @PreAuthorize("hasRole('ROLE_USER')")
     public Iterable<CategoryDto> getAll() {
         return categoryService.getAll();
     }
