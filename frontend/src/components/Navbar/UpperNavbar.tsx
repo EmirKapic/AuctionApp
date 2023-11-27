c5d8e01d4323f373a8a4daeeb9a796d003b42528
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-27 14:39:10 +0100

    navbar right text logic

--- frontend/src/components/Navbar/UpperNavbar.tsx
+++ frontend/src/components/Navbar/UpperNavbar.tsx
@@ -2,12 +2,21 @@ import Container from "components/Common/Container";
 import SocialMediaIcon from "components/Navbar/SocialMediaIcon";
 import { UserContext } from "contexts/UserContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 export default function UpperNavbar() {
   const userContext = useContext(UserContext);
   return (
     <section className="w-full py-2 bg-grey_">
       <Container className="flex justify-between items-center" type="large">
+        <button
+          className="text-white"
+          onClick={() => {
+            localStorage.clear();
+          }}
+        >
+          clear local storage
+        </button>
         <nav>
           <ul className="flex items-center gap-4">
             <a href="https://bs-ba.facebook.com/Emir.Kapic02">
@@ -22,9 +31,17 @@ export default function UpperNavbar() {
             </a>
           </ul>
         </nav>
-        <div className="text-white">
-          Hi, <span>{userContext?.firstName || "John"}</span>
-        </div>
+        {userContext ? (
+          <div className="text-white">
+            Hi, <span>{userContext?.firstName || "John"}</span>
+          </div>
+        ) : (
+          <div className="text-white flex gap-3">
+            <Link to={"/account/login"}>Login</Link>
+            <span className="text-lightgrey-200">or</span>
+            <Link to={"/account/register"}>Create an account</Link>
+          </div>
+        )}
       </Container>
     </section>
   );
