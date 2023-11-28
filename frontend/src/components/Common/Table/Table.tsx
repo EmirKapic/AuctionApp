132a5f4cd67ebdc6edeb548fb9fb16c7af404970
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-28 22:31:40 +0100

    add table component

---
+++ frontend/src/components/Common/Table/Table.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+export interface TableProps {
+  headers?: ReactNode[];
+  content?: ReactNode[];
+  rowClassName?: string;
+  headerClassName?: string;
+}
+
+export default function Table(props: TableProps) {
+  return (
+    <div className="w-full">
+      <table className="w-full">
+        <thead className={props.headerClassName + " " + props.rowClassName}>
+          {props.headers}
+        </thead>
+        <tbody>{props.content}</tbody>
+      </table>
+    </div>
+  );
+}
