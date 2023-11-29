6ee3b4af9c9d0cd3ab0df2332c92c724513b4331
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-29 14:54:19 +0100

    add item add btn to sell tab

--- frontend/src/pages/UserProfile/Seller.tsx
+++ frontend/src/pages/UserProfile/Seller.tsx
@@ -3,8 +3,9 @@ import { ReactNode, useContext, useState } from "react";
 import BidTable from "./BidTable";
 import UrlBuilder from "services/UrlBuilder";
 import NotSelling from "./NotSelling";
-import { act } from "react-dom/test-utils";
 import Button from "components/Common/Button";
+import Icon from "svgs/Icon";
+import { useNavigate } from "react-router-dom";
 
 function renderTabButton(
   label: string,
@@ -29,6 +30,7 @@ function renderTabButton(
 export default function Seller() {
   const [activeTab, setActiveTab] = useState<0 | 1>(0);
   const userContext = useContext(UserContext);
+  const navigate = useNavigate();
   const fetchUrl = new UrlBuilder()
     .bids()
     .user()
@@ -46,7 +48,14 @@ export default function Seller() {
           {renderTabButton("Active", activeTab === 0, () => setActiveTab(0))}
           {renderTabButton("Sold", activeTab === 1, () => setActiveTab(1))}
         </div>
-        <Button type="primary">hi</Button>
+        <Button
+          type="primary"
+          className="py-2 px-16 mb-3"
+          onClick={() => navigate("/")}
+        >
+          <Icon name="plus" />
+          Add item
+        </Button>
       </div>
       {activeTab === 0 ? (
         <BidTable
