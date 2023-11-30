53fd9e34d1921e5eb692240c081bab78a247335b
Emir Kapic <ekapic1@etf.unsa.ba>
2023-11-30 14:37:23 +0100

    stop button submiting forms

--- frontend/src/components/Common/Button.tsx
+++ frontend/src/components/Common/Button.tsx
@@ -1,12 +1,13 @@
 import { MouseEventHandler, PropsWithChildren } from "react";
 import { className } from "services/ClassName";
 
-type ButtonType = "primary" | "primary-filled";
+type ButtonType = "primary" | "primary-filled" | "secondary";
 
 export interface ButtonProps {
   type: ButtonType;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  formButtonType?: "submit" | "reset" | "button";
 }
 
 const buttonClassName: Record<ButtonType, string> = {
@@ -14,6 +15,8 @@ const buttonClassName: Record<ButtonType, string> = {
     "border-2 border-purple flex gap-2 items-center justify-center duration-300 hover:bg-purple hover:text-white shadow-purple-md",
   "primary-filled":
     "flex gap-2 items-center justify-center bg-purple text-white",
+  secondary:
+    "border-2 border-silver flex gap-2 items-center justify-center duration-300",
 };
 
 export default function Button(props: PropsWithChildren<ButtonProps>) {
@@ -21,6 +24,7 @@ export default function Button(props: PropsWithChildren<ButtonProps>) {
     <button
       className={className(buttonClassName[props.type], props.className)}
       onClick={props.onClick}
+      type={props.formButtonType}
     >
       {props.children}
     </button>
