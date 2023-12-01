1e53fbf7492df59b7120646c2f8efd89b6b4fdf0
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-01 14:23:01 +0100

    add drag and drop component

---
+++ frontend/src/components/Common/DragAndDrop.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from "react";
+import { useDropzone } from "react-dropzone";
+import { useFormContext } from "react-hook-form";
+
+export interface DragAndDropProps {
+  onDrop: (acceptedFiles: Array<File>) => void;
+  className?: string;
+}
+
+export default function DragAndDrop(
+  props: PropsWithChildren<DragAndDropProps>,
+) {
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: props.onDrop,
+  });
+  return (
+    <div {...getRootProps()} className={props.className}>
+      <input {...getInputProps()} id="drop" />
+      {props.children}
+    </div>
+  );
+}
