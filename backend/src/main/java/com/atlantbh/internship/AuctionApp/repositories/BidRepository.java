abdf91c1a10a2092ac606d8c9ecebfb72fc2c29e
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-04 20:26:15 +0100

    remove unneeded methods

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/BidRepository.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/BidRepository.java
@@ -5,11 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.Instant;
-
 public interface BidRepository extends JpaRepository<Bid, Long> {
     Page<Bid> findAllByBidderId(long bidderId, Pageable pageable);
-    /* these 2 below could be written as one query if i write it myself but this way hibernate does everything :)*/
-    Page<Bid> findAllByProduct_UserIdAndProduct_DateEndAfter(long sellerId, Pageable pageable, Instant date);
-    Page<Bid> findAllByProduct_UserIdAndProduct_DateEndBefore(long sellerId, Pageable pageable, Instant date);
+
 }
