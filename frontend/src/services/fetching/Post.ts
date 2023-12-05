4728b7ba09bbdb2317f3a42fa6eaa1585cc80f6c
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-05 21:46:28 +0100

    auth headers now applied by default

--- frontend/src/services/fetching/Post.ts
+++ frontend/src/services/fetching/Post.ts
@@ -1,14 +1,18 @@
 import { FetchReturnType, fetchData } from "./FetchData";
 import { HttpOptions } from "./Options";
+import { getAuthorizationHeaders } from "services/UserAuth";
 
 export default function post<R, B = any>(
   url: string,
   requestBody: B,
   options?: HttpOptions,
 ): Promise<FetchReturnType<R>> {
+  const completeHeaders = new Headers(getAuthorizationHeaders());
+  options?.headers.forEach((val, key) => completeHeaders.append(key, val));
+
   return fetchData<R, B>(url, {
     method: "POST",
-    headers: options?.headers,
+    headers: completeHeaders,
     body: requestBody,
   });
 }
