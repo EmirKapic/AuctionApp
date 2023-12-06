f7d45b17654b572d0ff7aac905647ea77836fe90
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-06 19:31:07 +0100

    add query params to useFetchOne hook

--- frontend/src/hooks/useFetchOne.tsx
+++ frontend/src/hooks/useFetchOne.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { getAuthorizationHeaders } from "services/UserAuth";
 
-export default function useFetchOne<T>(url: string) {
+export default function useFetchOne<T>(
+  url: string,
+  queryParams?: URLSearchParams,
+) {
   const [data, setData] = useState<T>();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    const completeUrl = url + (queryParams ? `?${queryParams.toString()}` : "");
     const fetchData = async () => {
       try {
-        const res = await fetch(url, {
+        const res = await fetch(completeUrl, {
           headers: getAuthorizationHeaders(),
         });
         if (!res.ok) {
