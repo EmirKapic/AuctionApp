41e3f250f60eb9505eb186e3902048b4b73f5b26
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-07 11:37:03 +0100

    bid repository now only shows highest bid for some product

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/BidRepository.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/BidRepository.java
@@ -10,8 +10,13 @@ import org.springframework.data.repository.query.Param;
 public interface BidRepository extends JpaRepository<Bid, Long> {
 
     @Query("""
-            from Bid where (:bidderEmail is null or bidder.email = :bidderEmail)
-            and (:productId is null or product.id = :productId)
+            select b from Bid b
+            where b.bid =
+                (select max(b1.bid)
+                from Bid b1
+                where b1.product.id = b.product.id)
+           and(:bidderEmail is null or :bidderEmail = b.bidder.email)
+           and(:productId is null or b.product.id = :productId)
             """)
     Page<Bid> findAllBids(@Param("bidderEmail") String bidderEmail,
             @Param("productId") Long productId,
