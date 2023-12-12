e3adc16e50669a0a1552af93e7f1ac98e62299d6
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-12 11:37:42 +0100

    im dumb

--- frontend/src/services/fileManaging/FirebaseFileManager.ts
+++ frontend/src/services/fileManaging/FirebaseFileManager.ts
@@ -1,10 +1,10 @@
-import app from "firebase/Firebase";
+import firebase from "firebase/Firebase";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import FileManager from "./FileManager";
 
 class FirebaseFileManager implements FileManager {
   uploadFile(file: File): Promise<string> {
-    app;
+    firebase.initializeFirebase();
     const storage = getStorage();
     const storageRef = ref(storage, URL.createObjectURL(file));
     return uploadBytes(storageRef, file).then(() => {
