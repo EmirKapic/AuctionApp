c37784e3640182c7d1daebd213ff598f23840c34
Emir Kapic <ekapic1@etf.unsa.ba>
2023-12-19 16:15:32 +0100

    more date shenanigans

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/ProductRepository.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/repositories/ProductRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
         @Query("""
                         from Product
-                        where (cast(dateStart as date) <= current_date) and dateEnd > current date
+                        where dateStart <= current_timestamp and dateEnd > current_timestamp
                                 and user.email <> :userEmail
                                 and purchased=false
                         order by RANDOM() LIMIT 1
@@ -27,8 +27,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                         and (:excludedSeller is null or user.email <> :excludedSeller)
                         and
                             case
-                                when :active = true then ((cast(dateStart as date) <= current_date) and dateEnd > current date and purchased=false)
-                                when :active = false then (dateEnd < current date)
+                                when :active = true then (dateStart <= current_timestamp and dateEnd > current_timestamp and purchased=false)
+                                when :active = false then (dateEnd < current_timestamp)
                                 else true
                             end
                         """)
@@ -44,7 +44,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                         from Product where (:categoryId is null or :categoryId = subCategory.category.id )
                         and (:subcategoryId is null or :subcategoryId = subCategory.id)
                         and (:name is null or levenshtein(upper(name),upper(:name)) <= 3)
-                        and ((cast(dateStart as date) <= current_date) and dateEnd > current date)
+                        and (dateStart<= current_timestamp and dateEnd > current_timestamp)
                         and purchased=false
                         order by levenshtein(upper(name), upper(:name))""")
         Page<Product> getAllActiveApproximate(Pageable pageable,
@@ -60,8 +60,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                                  join user_seller_interaction as sellerInteraction on sellerInteraction.seller_id = product.seller_id
                                  where subInteraction.user_id = :userId and sellerInteraction.user_id = :userId
                                          and product.purchased = false
-                                         and (cast(product.date_start as date) <= current_date)
-                                         and product.date_end > current_date
+                                         and (product.date_start <= current_timestamp)
+                                         and product.date_end > current_timestamp
                                          and product.seller_id <> :userId
                                  order by ((subInteraction.views * case
                                                                          when subInteraction.last_interacted_with < current_date - interval '2 months' then 1
@@ -79,6 +79,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                          """, nativeQuery = true)
         List<Product> getRecommendedProducts(@Param("userId") long userId);
 
-        @Query("from Product where dateEnd > current date and purchased=false and :sellerEmail <> user.email order by RANDOM() limit 3")
+        @Query("""
+                from Product
+                where dateStart < current_timestamp
+                        and dateEnd > current_timestamp
+                        and purchased=false
+                        and :sellerEmail <> user.email
+                order by RANDOM()
+                limit 3""")
         List<Product> findTop3ByRandom(String sellerEmail);
 }
