99693a0fd0a9ebc93f8224f08dadfa81eb76d26c
Emir Kapic <ekapic1@etf.unsa.ba>
2024-01-14 20:33:30 +0100

    post merge fix

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/models/User.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/models/User.java
@@ -61,12 +61,16 @@ public class User implements UserDetails {
     }
 
     public User(String email, String password, String firstName, String lastName, String authenticationMethod) {
-        this(0, firstName, lastName, email, password, null, null, null, authenticationMethod);
+        this(0, firstName, lastName, email, password, null, null, null,
+                null, null, null, null, null, null, null, false,
+                authenticationMethod
+        );
     }
 
     public User(String email, String role, String authenticationMethod) {
-        this(0, null, null, email, null, null, null, null, null, null, null, null, null, null, role,
-                authenticationMethod, false);
+        this(0, null, null, email, null, null, null,
+                role, null, null, null, null, null, null,
+                null, false, authenticationMethod);
     }
 
     @Override
