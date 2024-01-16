14a961bb68b62e61cc184d2fbec93d804dda4881
Emir Kapic <ekapic1@etf.unsa.ba>
2024-01-16 17:53:11 +0100

    maybe3

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/config/WebConfig.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/config/WebConfig.java
@@ -10,10 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**");
     }
 
 }
